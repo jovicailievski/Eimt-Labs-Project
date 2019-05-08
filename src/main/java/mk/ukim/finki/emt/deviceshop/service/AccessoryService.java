4367d69a863e88fe43a1a47dff82f62735b2d41f
jovicailievski <jovicailievski97@gmail.com>
2019-05-08 17:25:31 +0200

    finished

---
+++ src/main/java/mk/ukim/finki/emt/deviceshop/service/AccessoryService.java
@@ -0,0 +1,4 @@
+package mk.ukim.finki.emt.deviceshop.service;
+
+public interface AccessoryService {
+}
---
+++ src/main/java/mk/ukim/finki/emt/deviceshop/service/impl/AccessoryServiceImpl.java
@@ -0,0 +1,16 @@
+package mk.ukim.finki.emt.deviceshop.service.impl;
+
+
+import mk.ukim.finki.emt.deviceshop.repository.PersistentAccessoryRepository;
+import mk.ukim.finki.emt.deviceshop.service.AccessoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccessoryServiceImpl implements AccessoryService{
+
+    @Autowired
+    private PersistentAccessoryRepository accessoryRepository;
+
+    
+}
